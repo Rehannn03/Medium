652bd8fd5858ba369386d43709006cfa12b77fd8
REHAN SHAH <spamhomeacc123@gmail.com>
2024-07-11 01:53:20 +0530

    chore: Add Flowbite plugin to Tailwind CSS configuration

---
+++ frontend/src/components/BlogCard.tsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+
+interface BlogCardProps {
+    id: string,
+    title: string,
+    content: string,
+    authorName: string,
+    datePublished: string,
+}
+
+
+const BlogCard = ({ id,title, content, authorName,datePublished }:BlogCardProps) => {
+    return (
+    <Link to={`/blog/${id}`}>
+      <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl hover:shadow-2xl m-3">
+        <div className="md:flex">
+          <div className="p-8">
+            <div className="uppercase text-xl font-semibold pt-2">{title}</div>
+            <p className="mt-2 text-base text-gray-500">{content.substring(0, 100)}...</p>
+            <div className="mt-4 flex items-center">
+                <div className="h-10 w-10 rounded-full bg-slate-400 flex items-center justify-center mr-4">
+                    <span className="text-white text-xl">{authorName.charAt(0).toUpperCase()}</span>
+                </div>
+              <div className="text-sm">
+                <p className="text-gray-900 leading-none font-light">{authorName}</p>
+                <p className="text-gray-600">{datePublished}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </Link>
+    );
+  };
+
+  export default BlogCard;
+  
---
+++ frontend/src/hooks/index.ts
@@ -0,0 +1,59 @@
+import { useEffect,useState } from "react";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
+
+export interface Blog{
+    "content":string,
+    "title":string,
+    "id":string,
+    "author":{
+        "name":string
+    }
+}
+
+
+export function useBlog({id}:{id:string}){
+    const[loading,setLoading]=useState(true)
+    const[blog,setBlog]=useState<Blog[]>()
+
+    useEffect(()=>{
+        try {
+            axios.get(`${BACKEND_URL}/api/v1/blog/${id}`, {
+                headers: {
+                    Authorization: localStorage.getItem('token')
+                }
+            }).then((res)=>{
+                setBlog(res.data.blog)
+                setLoading(false)
+            })
+        } catch (error) {
+            console.log("error in fetching blog",error);
+            
+        }
+    },[id])
+
+    return {loading,blog}
+}
+
+export function useBlogs(){
+    const[loading,setLoading]=useState(true)
+    const[blogs,setBlogs]=useState<Blog[]>()
+
+    useEffect(()=>{
+        try {
+            axios.get(`${BACKEND_URL}/api/v1/blog/bulk`, {
+                headers: {
+                    Authorization: localStorage.getItem('token')
+                }
+            }).then((res)=>{
+                setBlogs(res.data.blogs)
+                setLoading(false)
+            })
+        } catch (error) {
+            console.log("error in fetching blog",error);
+            
+        }
+    },[])
+
+    return {loading,blogs}
+}
